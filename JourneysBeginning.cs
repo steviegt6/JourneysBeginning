9231dee06d188bc3ccea1b109c7e2c67d175feb0
Stevie <xxlennygamerxx@gmail.com>
2020-12-27 17:35:42 -0800

    Add Libvaxy as a reference & dependency

--- JourneysBeginning.cs
+++ JourneysBeginning.cs
@@ -1,8 +1,18 @@
+using System.Reflection;
 using Terraria.ModLoader;
 
 namespace JourneysBeginning
 {
-	public class JourneysBeginning : Mod
-	{
-	}
+    public class JourneysBeginning : Mod
+    {
+        public static JourneysBeginning Instance { get; private set; }
+
+        public static Assembly TMLAssembly { get; private set; }
+
+        public JourneysBeginning()
+        {
+            Instance = this;
+            TMLAssembly = typeof(ModLoader).Assembly;
+        }
+    }
 }
